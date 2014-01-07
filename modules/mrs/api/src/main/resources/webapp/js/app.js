24ac651f0cbe8fad57608b3163305690f3a7cfbf
Kamil Knitter <kknitter@soldevelo.com>
2014-01-07 10:49:40 +0100

    BUG: Javascript Error in Tasks
    https://trello.com/c/M1cOBrE4
    
    This fix removes 'bootstrap' module dependencies from
    angular app modules. Required due to removal of
    angular-bootstrap.js:
    http://review.motechproject.org/#/c/3544/
    
    Change-Id: I66cdb039b6a2043c5160345f7d3305f6614ecff1

--- modules/mrs/api/src/main/resources/webapp/js/app.js
+++ modules/mrs/api/src/main/resources/webapp/js/app.js
@@ -3,7 +3,7 @@
 
     /* App Module */
 
-    angular.module('motech-mrs', ['motech-dashboard', 'patientService', 'ngCookies', 'ngRoute', 'bootstrap', 'motech-widgets']).config(['$routeProvider',
+    angular.module('motech-mrs', ['motech-dashboard', 'patientService', 'ngCookies', 'ngRoute', 'motech-widgets']).config(['$routeProvider',
         function ($routeProvider) {
             $routeProvider.
                 when('/patients', {templateUrl: '../mrs/resources/partials/patients.html', controller: 'PatientMrsCtrl'}).

2f85dc682a63157db2cb54979e314af1fb746f19
Paweł Gesek <pgesek@soldevelo.com>
2014-04-08 14:08:54 +0200

    MOTECH-851: Transition to Pax-exam
    This commit makes the platform-medical-records repo use
    the new motech-pax-it for its OSGi tests.
    
    Change-Id: I735c9443bef02960530cb9f2c53a6e1a6a5e1a2d

--- modules/mrs/api/src/test/java/org/motechproject/mrs/osgi/MRSBundleIT.java
+++ modules/mrs/api/src/test/java/org/motechproject/mrs/osgi/MRSBundleIT.java
@@ -1,41 +1,41 @@
 package org.motechproject.mrs.osgi;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.motechproject.commons.api.DataProvider;
-import org.motechproject.event.listener.EventListenerRegistryService;
 import org.motechproject.mrs.MRSDataProvider;
 import org.motechproject.mrs.domain.MRSFacility;
 import org.motechproject.mrs.domain.MRSPatient;
 import org.motechproject.mrs.domain.MRSPerson;
-import org.motechproject.testing.osgi.BaseOsgiIT;
+import org.motechproject.testing.osgi.BasePaxIT;
+import org.motechproject.testing.osgi.helper.ServiceRetriever;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.osgi.framework.BundleContext;
 
+import javax.inject.Inject;
 import java.util.Arrays;
 import java.util.List;
 
-import static java.util.Arrays.asList;
+import static org.junit.Assert.assertTrue;
 
-public class MRSBundleIT extends BaseOsgiIT {
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class MRSBundleIT extends BasePaxIT {
 
+    @Inject
+    private BundleContext bundleContext;
+
+    @Test
     public void testMRSApiBundle() {
-        getService(EventListenerRegistryService.class);
-
-        MRSDataProvider providerLookup = (MRSDataProvider) getService(DataProvider.class);
+        MRSDataProvider providerLookup =
+                (MRSDataProvider) ServiceRetriever.getService(bundleContext, DataProvider.class);
 
         List<Class<?>> classes = Arrays.asList(MRSPerson.class, MRSPatient.class, MRSFacility.class);
 
         for (Class<?> cls : classes) {
             assertTrue(providerLookup.supports(cls.getSimpleName()));
         }
-    }
-
-    @Override
-    protected String[] getConfigLocations() {
-        return new String[]{"/META-INF/osgi/testApplicationMrsBundle.xml"};
-    }
-
-    @Override
-    protected List<String> getImports() {
-        return asList(
-                "org.motechproject.mrs"
-        );
     }
 }

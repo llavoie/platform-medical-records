b61d2b6941a6a86127506325f8e135c178ef5c08
Koshal Thirumalai <kosh@koshal.org>
2014-05-15 17:12:07 -0700

    Added stubs for scheduler integration

--- modules/appointments/src/main/java/org/motechproject/appointments/reminder/AppointmentReminderService.java
+++ modules/appointments/src/main/java/org/motechproject/appointments/reminder/AppointmentReminderService.java
@@ -1,7 +1,10 @@
 package org.motechproject.appointments.reminder;
 
+import org.joda.time.Period;
 import org.motechproject.appointments.domain.Appointment;
+import org.motechproject.event.MotechEvent;
 import org.motechproject.scheduler.MotechSchedulerService;
+import org.motechproject.scheduler.domain.RepeatingSchedulableJob;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +14,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppointmentReminderService {
 
-    private static final String SUBJECT = "Appointment.Reminder";
+    private static final String SUBJECT = "Appointment.Reminder.%s.%s";
     private MotechSchedulerService schedulerService;
 
 
@@ -30,5 +33,17 @@ public class AppointmentReminderService {
 
     public void updateReminders(Appointment appointment) {
 
+    }
+
+    private RepeatingSchedulableJob buildJob(Appointment appointment) {
+        String eventTitle = String.format(SUBJECT, appointment.getExternalId(), appointment.getId());
+        MotechEvent event = new MotechEvent(eventTitle);
+
+        // temp workaround for lack of period support in scheduler, ignoring anything bigger than weeks for now
+        Period interval = appointment.getReminderInterval();
+        long reminderInterval = interval.getWeeks() * 7 * 24 * 60 * 60 * 1000 + interval.getDays() * 24 * 60 * 60 * 1000 +
+                interval.getHours() * 60 * 60 * 1000 + interval.getMinutes() * 60 * 1000 + interval.getSeconds() * 1000 + interval.getMillis();
+
+        return new RepeatingSchedulableJob();
     }
 }
